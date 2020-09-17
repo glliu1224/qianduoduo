0b2d646b3f90fddb1c4b0cbfe3fc0a7c49988511
m13271319136@163.com <m13271319136@163.com>
2020-09-17 09:25:40 +0800

    映射文件修改

---
+++ src/main/java/com/exprise/LombokPluginApplication.java
@@ -0,0 +1,13 @@
+package com.exprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokPluginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LombokPluginApplication.class, args);
+    }
+
+}
